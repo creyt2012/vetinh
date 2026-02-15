920e2654b488323bbdaa6a8fb49e762c110c02de
creyt2012 <mortarcloud@gmail.com>
2026-02-16 06:46:34 +0700

    Auto-sync: 2026-02-16 06:46:34

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\LiveStateController;
+use App\Http\Controllers\Api\V1\SatelliteController;
+use App\Http\Controllers\Api\V1\WeatherController;
 
 Route::middleware(['auth.api_key'])->prefix('v1')->group(function () {
     Route::get('/live/state', [LiveStateController::class, 'index']);
