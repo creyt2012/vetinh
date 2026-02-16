2c9095a2396b30d0fce10c77964551f3b40b1f1d
creyt2012 <mortarcloud@gmail.com>
2026-02-17 06:36:19 +0700

    Ultra-sync: app/Http/Controllers/User/ApiKeyController.php (06:36:19.3N)

--- app/Http/Controllers/User/ApiKeyController.php
+++ app/Http/Controllers/User/ApiKeyController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\ApiKey;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Support\Facades\Auth;
 
 class ApiKeyController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        return Inertia::render('User/ApiKeys/Index', [
+            'apiKeys' => ApiKey::where('tenant_id', Auth::user()->tenant_id)->get()
+        ]);
+    }
 }
