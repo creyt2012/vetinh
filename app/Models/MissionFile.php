516202128fd76dc406d49919fd6c12a6bcf96e06
creyt2012 <mortarcloud@gmail.com>
2026-02-17 05:37:55 +0700

    Auto-sync: app/Models/MissionFile.php (2026-02-17 05:37:54)

--- app/Models/MissionFile.php
+++ app/Models/MissionFile.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class MissionFile extends Model
 {
-    //
+    protected $fillable = [
+        'tenant_id',
+        'original_name',
+        'stored_path',
+        'mime_type',
+        'type',
+        'status',
+        'error_message',
+        'processed_at',
+        'metadata'
+    ];
+
+    protected $casts = [
+        'metadata' => 'array',
+        'processed_at' => 'datetime'
+    ];
+
+    public function tenant()
+    {
+        return $this->belongsTo(Tenant::class);
+    }
 }
