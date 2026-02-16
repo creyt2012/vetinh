ce52aa50bbb1f4f17412cd75e81f6133761bfc34
creyt2012 <mortarcloud@gmail.com>
2026-02-16 07:26:21 +0700

    Auto-sync: 2026-02-16 07:26:21

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Dashboard');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/satellites', [\App\Http\Controllers\Admin\SatelliteManagementController::class, 'index'])->name('admin.satellites.index');
+    Route::post('/satellites', [\App\Http\Controllers\Admin\SatelliteManagementController::class, 'store'])->name('admin.satellites.store');
+    Route::put('/satellites/{satellite}', [\App\Http\Controllers\Admin\SatelliteManagementController::class, 'update'])->name('admin.satellites.update');
+    Route::delete('/satellites/{satellite}', [\App\Http\Controllers\Admin\SatelliteManagementController::class, 'destroy'])->name('admin.satellites.destroy');
+});
