b23ca5236dbc9806f766ec1f8462d0d523e923bd
creyt2012 <mortarcloud@gmail.com>
2026-02-17 05:23:44 +0700

    Auto-sync: database/seeders/GroundStationSeeder.php (2026-02-17 05:23:43)

--- database/seeders/GroundStationSeeder.php
+++ database/seeders/GroundStationSeeder.php
@@ -12,6 +12,16 @@ class GroundStationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $stations = [
+            ['name' => 'Hanoi Meteo Hub', 'code' => 'HAN-MET-01', 'latitude' => 21.0285, 'longitude' => 105.8542, 'type' => 'METEO'],
+            ['name' => 'Da Nang Coastal Radar', 'code' => 'DAD-RAD-02', 'latitude' => 16.0544, 'longitude' => 108.2022, 'type' => 'RADAR'],
+            ['name' => 'SGN Mission Control', 'code' => 'SGN-MC-01', 'latitude' => 10.8231, 'longitude' => 106.6297, 'type' => 'MISSION_CONTROL'],
+            ['name' => 'Kourou Spaceport', 'code' => 'GUY-SPACE-01', 'latitude' => 5.236, 'longitude' => -52.768, 'type' => 'MISSION_CONTROL'],
+            ['name' => 'Svalbard Ground Station', 'code' => 'SVA-GND-01', 'latitude' => 78.2298, 'longitude' => 15.6469, 'type' => 'METEO'],
+        ];
+
+        foreach ($stations as $station) {
+            \App\Models\GroundStation::updateOrCreate(['code' => $station['code']], $station);
+        }
     }
 }
