cbc0d3180720778768867cbf74b4e41773995b1b
creyt2012 <mortarcloud@gmail.com>
2026-02-17 05:15:45 +0700

    Auto-sync: app/Models/Subscription.php (2026-02-17 05:15:45)

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscription extends Model
 {
-    //
+    protected $fillable = [
+        'tenant_id',
+        'plan_id',
+        'status',
+        'gateway',
+        'gateway_id',
+        'trial_ends_at',
+        'ends_at'
+    ];
+
+    protected $casts = [
+        'trial_ends_at' => 'datetime',
+        'ends_at' => 'datetime',
+    ];
+
+    public function tenant()
+    {
+        return $this->belongsTo(Tenant::class);
+    }
+
+    public function plan()
+    {
+        return $this->belongsTo(Plan::class);
+    }
 }
