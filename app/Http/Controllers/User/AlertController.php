af459940de6fec2147f481f05755bbc03a57c7ff
creyt2012 <mortarcloud@gmail.com>
2026-02-17 06:36:16 +0700

    Ultra-sync: app/Http/Controllers/User/AlertController.php (06:36:16.3N)

--- app/Http/Controllers/User/AlertController.php
+++ app/Http/Controllers/User/AlertController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AlertController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        return Inertia::render('User/Alerts/Index', [
+            'alerts' => Auth::user()->tenant ? Auth::user()->tenant->activityLogs()->where('action', 'NOTIFICATION_SENT')->latest()->limit(50)->get() : []
+        ]);
+    }
 }
