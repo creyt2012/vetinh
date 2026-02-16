b19f456b5bb70c17fd75c6d5666f89822076a193
creyt2012 <mortarcloud@gmail.com>
2026-02-17 05:18:41 +0700

    Auto-sync: config/billing.php (2026-02-17 05:18:38)

---
+++ config/billing.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'default' => env('BILLING_GATEWAY', 'stripe'),
+    'gateways' => [
+        'stripe' => [
+            'key' => env('STRIPE_KEY'),
+            'secret' => env('STRIPE_SECRET'),
+            'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
+        ],
+        'paypal' => [
+            'client_id' => env('PAYPAL_CLIENT_ID'),
+            'secret' => env('PAYPAL_SECRET'),
+            'mode' => env('PAYPAL_MODE', 'sandbox'),
+        ],
+    ],
+];
