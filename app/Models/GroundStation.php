3ee0c43ffe19ff37e5fea679ae0b1a0c5eea7d17
creyt2012 <mortarcloud@gmail.com>
2026-02-17 05:23:11 +0700

    Auto-sync: app/Models/GroundStation.php (2026-02-17 05:23:10)

--- app/Models/GroundStation.php
+++ app/Models/GroundStation.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class GroundStation extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'code',
+        'latitude',
+        'longitude',
+        'type',
+        'status',
+        'metadata'
+    ];
+
+    protected $casts = [
+        'metadata' => 'array',
+        'latitude' => 'float',
+        'longitude' => 'float',
+    ];
+
+    public function weatherMetrics()
+    {
+        return $this->hasMany(WeatherMetric::class, 'station_id');
+    }
+
+    public function latestMetric()
+    {
+        return $this->hasOne(WeatherMetric::class, 'station_id')->latestOfMany();
+    }
 }
