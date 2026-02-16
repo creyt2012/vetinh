9624024ffffff12fbd78b14c6b95281027a9b761
creyt2012 <mortarcloud@gmail.com>
2026-02-16 07:29:34 +0700

    Auto-sync: 2026-02-16 07:29:34

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('local')) {
+            error_reporting(error_reporting() & ~E_DEPRECATED);
+        }
     }
 }
