98b7a43420a37dc138e1cf0448858a00afc86b6f
creyt2012 <mortarcloud@gmail.com>
2026-02-17 05:25:15 +0700

    Auto-sync: routes/console.php (2026-02-17 05:25:13)

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,8 @@ use Illuminate\Support\Facades\Schedule;
 use App\Jobs\HimawariIngestJob;
 use App\Jobs\SatellitePropagateJob;
 
+use App\Jobs\GroundIngestJob;
+
 Schedule::job(new HimawariIngestJob)->everyTenMinutes();
 Schedule::job(new SatellitePropagateJob)->everyFiveSeconds();
+Schedule::job(new GroundIngestJob)->everyMinute();
